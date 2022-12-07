a3d3446dc1b151f35c8172dccf3805dadac40bdf
benjamin kunkel <benjamink1125@gmail.com>
2022-12-06 17:34:34 -0800

    initial commit

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class IProductRepository
+    {
+        public IProductRepository();
+            public IEnumerable<Product> GetAllProducts();
+        public ProductRepositoryusingSystem.Data;
+        {
+         
+        }
+    }
+    public ProductRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+}
+public IEnumerable<Product> GetAllProducts()
+{
+    return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+}
+
+
---
+++ Testing/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+         public Product()
+        {
+  
+    }
+
+        public int productID { get; set;}
+        public int Name { get; set; }
+        public int Price { get; set; }
+        public int catagoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+    }
+}
+
